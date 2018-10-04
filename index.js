5ab54ede9a1f0e1d583759ce310196e939f6748e
bogdanned <bogdan.nedelcu@lokallead.de>
2018-10-04 15:18:00 +0200

    addd https suport

--- index.js
+++ index.js
@@ -2,23 +2,48 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const axios = require("axios");
+const fs = require('fs');
+const https = require('https');
 
-const port = process.env.PORT || 4001;
-const index = require("./routes/index");
+const privateKey  = fs.readFileSync('certs/server.key', 'utf8');
+const certificate = fs.readFileSync('certs/server.crt', 'utf8');
 
+const credentials = {key: privateKey, cert: certificate};
+
+// creating app
 const app = express();
+
+const index = require("./routes/index");
 app.use(index);
 
-const server = http.createServer(app);
-const io = socketIo(server);
+// creating servers
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
-io.on("connection", socket => {
+// http port defaults
+const httpPort = process.env.PORT || 8080;
+const httpsPort = process.env.PORT || 8443;
+
+// adding io framework
+const ioHttp = socketIo(httpServer);
+const ioHttps = socketIo(httpsServer);
+
+
+ioHttp.on("connection", socket => {
   console.log("New client connected"), setInterval(
     () => getApiAndEmit(socket),
     10000
   );
   socket.on("disconnect", () => console.log("Client disconnected"));
 });
+
+ioHttps.on("connection", socket => {
+    console.log("New client connected"), setInterval(
+      () => getApiAndEmit(socket),
+      10000
+    );
+    socket.on("disconnect", () => console.log("Client disconnected"));
+  });
 
 const getApiAndEmit = async socket => {
   try {
@@ -31,4 +56,7 @@ const getApiAndEmit = async socket => {
   }
 };
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+
+// launching servers
+httpServer.listen(httpPort, () => console.log(`Http server listening on port ${httpPort}`));
+httpsServer.listen(httpsPort, () => console.log(`Https server listening on port ${httpsPort}`));
