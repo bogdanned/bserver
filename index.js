9e55eb2325ebd215b468fc1e7b994000523fc2ce
bogdanned <bogdan.nedelcu@lokallead.de>
2018-10-04 15:06:11 +0200

    added socket io for straming

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", socket => {
+  console.log("New client connected"), setInterval(
+    () => getApiAndEmit(socket),
+    10000
+  );
+  socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
+const getApiAndEmit = async socket => {
+  try {
+    const res = await axios.get(
+      "https://api.darksky.net/forecast/PUT_YOUR_API_KEY_HERE/43.7695,11.2558"
+    );
+    socket.emit("FromAPI", res.data.currently.temperature);
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
