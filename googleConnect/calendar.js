a08109c24050eb0873c67eee80115e70b48d5efb
Raúl Berganza <brgraul@gmail.com>
2018-10-05 02:28:44 +0200

    Modification socket.io

--- googleConnect/calendar.js
+++ googleConnect/calendar.js
@@ -3,11 +3,11 @@ const readline = require('readline');
 const {google} = require('googleapis');
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 const TOKEN_PATH = 'token.json';
 
 // Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
+fs.readFile('./googleConnect/credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Calendar API.
   authorize(JSON.parse(content), listEvents);
@@ -69,23 +69,26 @@ function getAccessToken(oAuth2Client, callback) {
  */
 function listEvents(auth) {
   const calendar = google.calendar({version: 'v3', auth});
-  calendar.events.list({
+
+  calendar.events.get({
     calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
+    eventId: 'hllphucqo26q5qfhsc2spv3abg_20200925'
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
+    const event = res.data;
+    console.log(event)
+    event.description = 'birthday party'
+    calendar.events.update({
+      calendarId:"primary",
+      eventId: event.id,
+      event
+    })
   });
+
+
+}
+
+
+module.exports.getEventList = function getEventList(){
+  console.log('get called when post')
 }
